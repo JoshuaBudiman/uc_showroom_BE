e46b2ef5a71e07a3b42be9b528463928cf704290
JoshuaBudiman <joshua.kbudiman@gmail.com>
2023-11-13 20:05:01 +0800

    delete unnecessary files

--- Vehicles_actions.php
+++ Vehicles_actions.php
@@ -25,6 +25,12 @@ if ("CREATE_TABLE" == $action) {
         jumlah_penumpang INT NOT NULL,
         manufaktur VARCHAR (255) NOT NULL,
         harga FLOAT NOT NULL,
+        tipe_bahan_bakar VARCHAR (255) NULL,
+        luas_bagasi FLOAT NULL,
+        ukuran_bagasi FLOAT NULL,
+        kapasitas_bensin FLOAT NULL,
+        jumlah_roda INT NULL,
+        luas_kargo FLOAT NULL,
 
     )";
     if ($conn->query($sql) === TRUE) {
@@ -60,9 +66,7 @@ if('GET_VEH' == $action){
     $sql = "SELECT * FROM $table WHERE vehicle_id = $vehicle_id";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            $dbdata[]=$row;
-        }
+        $row = $result->fetch_assoc();
         echo json_encode($dbdata);
     } else {
         echo "error";
@@ -79,7 +83,15 @@ if('ADD_VEH' == $action){
     $jumlah_penumpang = $_POST['jumlah_penumpang'];
     $manufaktur = $_POST['manufaktur'];
     $harga = $_POST['harga'];
-    $sql = "INSERT INTO $table (jenis, model, tahun, jumlah_penumpang, manufaktur, harga) VALUES('$jenis', '$model', '$tahun, '$jumlah_penumpang', '$manufaktur', '$harga')";
+    $tipe_bahan_bakar = $_POST['tipe_bahan_bakar'];
+    $luas_bagasi = $_POST['luas_bagasi'];
+    $ukuran_bagasi = $_POST['ukuran_bagasi'];
+    $kapasitas_bensin = $_POST['kapasitas_bensin'];
+    $jumlah_roda = $_POST['jumlah_roda'];
+    $luas_kargo = $_POST['luas_kargo'];
+
+    $sql = "INSERT INTO $table (jenis, model, tahun, jumlah_penumpang, manufaktur, harga, tipe_bahan_bakar, luas_bagasi, ukuran_bagasi, kapasitas_bensin, jumlah_roda, luas_kargo) 
+    VALUES('$jenis', '$model', '$tahun, '$jumlah_penumpang', '$manufaktur', '$harga', '$tipe_bahan_bakar', '$luas_bagasi', '$ukuran_bagasi', '$kapasitas_bensin', '$jumlah_roda', '$luas_kargo')";
     $result = $conn->query($sql);
     echo 'success';
     return;
@@ -94,7 +106,15 @@ if('UPDATE_VEH' == $action){
     $jumlah_penumpang = $_POST['jumlah_penumpang'];
     $manufaktur = $_POST['manufaktur'];
     $harga = $_POST['harga'];
-    $sql = "UPDATE $table SET jenis = '$jenis', model = '$model', tahun = '$tahun', jumlah_penumpang = '$jumlah_penumpang', manufaktur = '$manufaktur', harga = '$harga' WHERE vehicle_id = $vehicle_id";
+    $tipe_bahan_bakar = $_POST['tipe_bahan_bakar'];
+    $luas_bagasi = $_POST['luas_bagasi'];
+    $ukuran_bagasi = $_POST['ukuran_bagasi'];
+    $kapasitas_bensin = $_POST['kapasitas_bensin'];
+    $jumlah_roda = $_POST['jumlah_roda'];
+    $luas_kargo = $_POST['luas_kargo'];
+    $sql = "UPDATE $table SET jenis = '$jenis', model = '$model', tahun = '$tahun', jumlah_penumpang = '$jumlah_penumpang', manufaktur = '$manufaktur', harga = '$harga', 
+    tipe_bahan_bakar = '$tipe_bahan_bakar', luas_bagasi = '$luas_bagasi', ukuran_bagasi = '$ukuran_bagasi', kapasitas_bensin = '$kapasitas_bensin', jumlah_roda = '$jumlah_roda', luas_kargo = '$luas_kargo',
+    WHERE vehicle_id = $vehicle_id";
     if ($conn->query($sql) === TRUE) {
         echo "success";
     } else {
